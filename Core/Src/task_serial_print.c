ac7ea8af289019f5d77d60413890a2151f3ab0fb
Damian Brzana <brzanad@gmail.com>
2023-12-16 02:46:57 +0100

    Removed unused code section, later might remove whole task

--- Core/Src/task_serial_print.c
+++ Core/Src/task_serial_print.c
@@ -13,17 +13,9 @@
 
 void start_task_serial_print(void *argument)
 {
-	uint8_t i = 0;
 	while (1)
 	{
-		i++;
-		osDelay(100);
-		if (i % 100 == 1)
-		{
-			printf("Hello, damn world! x%d\n", i);
-			//HAL_GPIO_WritePin(BUZZER_GPIO_Port, BUZZER_Pin, SET);
-		}
-		//if (i % 100 == 2) HAL_GPIO_WritePin(BUZZER_GPIO_Port, BUZZER_Pin, RESET);
+		osDelay(1000);
 	}
 }
 
